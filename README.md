72f8c7bdf7008d443af26138f2b1978810dc82c9
Deshmaneparas <141109408+Deshmaneparas@users.noreply.github.com>
2023-08-11 10:16:45 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# my-hero-15
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/stackblitz-starters-8dhwfi)
---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HerosComponent } from './heros/heros.component';
+
+@Component({
+  standalone:true,
+  selector: 'app-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  imports:[HerosComponent]
+})
+export class AppComponent implements OnInit {
+  // username:string="hiii";
+  // phone:string="8234673441";
+  // age:number=23;
+  // photo:string="https://www.thedrive.com/content/2018/09/img_0199.jpg?quality=85&crop=16%3A9&auto=webp&optimize=high&quality=70&width=1440"
+  title = 'Tour of Heroes';
+  
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
